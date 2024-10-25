5a8c032088409b35b86e005869e0853208bb6a12
ShutmanAY17 <ismaelestanislaoc@gmail.com>
2024-10-24 19:09:32 -0600

    Nuevo controlador para asignar recursos a una oferta donde el representante cliente fue aceptado

---
+++ src/main/java/com/ipn/mx/exeptions/RecursoInvalidoExeption.java
@@ -0,0 +1,9 @@
+package com.ipn.mx.exeptions;
+
+public class RecursoInvalidoExeption extends RuntimeException {
+
+	public RecursoInvalidoExeption(String message) {
+        super(message);
+    }
+	
+}
