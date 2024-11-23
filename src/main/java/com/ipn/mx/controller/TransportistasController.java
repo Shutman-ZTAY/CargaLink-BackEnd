02f90e67aea83d10f52a91d7763cdd8f9c267f46
ShutmanAY17 <ismaelestanislaoc@gmail.com>
2024-11-23 02:16:38 -0600

    Retorno de entidades

--- src/main/java/com/ipn/mx/controller/TransportistasController.java
+++ src/main/java/com/ipn/mx/controller/TransportistasController.java
@@ -98,7 +98,7 @@ public class TransportistasController {
 	}
 	
 	//RF06	Gestionar cuentas de transportistas
-	@GetMapping(value = {"/representante/transporte/transportista/{idTransportista}", "transportista/profile/{idTransportista}"})
+	@GetMapping(value = {"/representante/transporte/transportista/{idTransportista}", "/transportista/profile/{idTransportista}"})
 	public ResponseEntity<?> viewTransportistasById(@PathVariable String idTransportista){
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (ControllerUtils.isAuthorised(auth, RolUsuario.REPRESENTANTE_TRANSPORTE) || ControllerUtils.isAuthorised(auth, RolUsuario.TRANSPORTISTA)) {
