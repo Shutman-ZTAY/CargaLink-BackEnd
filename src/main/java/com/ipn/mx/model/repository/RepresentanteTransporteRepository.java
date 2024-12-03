9c9a8255c8cc2657b1d7306c94fa1debc5472720
ShutmanAY17 <ismaelestanislaoc@gmail.com>
2024-12-03 01:08:17 -0600

    Correccion en la capa de repossitorio

--- src/main/java/com/ipn/mx/model/repository/RepresentanteTransporteRepository.java
+++ src/main/java/com/ipn/mx/model/repository/RepresentanteTransporteRepository.java
@@ -7,12 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ipn.mx.model.entity.RepresentanteTransporte;
-import com.ipn.mx.model.enumerated.EstatusRepTrans;
 
 public interface RepresentanteTransporteRepository extends JpaRepository<RepresentanteTransporte, String> {
 	
 	@Query("SELECT rt FROM RepresentanteTransporte rt "
-			+ "WHERE rt.estatus = EstatusRepTrans.VALIDO")
+			+ "WHERE rt.estatusRepTrans = EstatusRepTrans.NO_VALIDO")
 	List<RepresentanteTransporte> findAllNotValid();
 
 	@Query("SELECT CASE WHEN (COUNT(rt) > 0) THEN TRUE ELSE FALSE END FROM RepresentanteTransporte rt "
