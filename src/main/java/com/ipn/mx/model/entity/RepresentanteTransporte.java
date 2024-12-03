4cbd3f2f029ba5d53d7032968c7ae50c7e3c1ae0
Shutman-ZTAY <ismaelestanislaoc@gmail.com>
2024-12-03 00:04:02 -0600

    correcion

--- src/main/java/com/ipn/mx/model/entity/RepresentanteTransporte.java
+++ src/main/java/com/ipn/mx/model/entity/RepresentanteTransporte.java
@@ -19,7 +19,6 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
